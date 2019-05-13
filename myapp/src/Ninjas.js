35c1d515f2bdb80cc25ce8c1f07c53cf14bc7904
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-13 01:55:03 -0400

    conditional output

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 const Ninjas = ({ninjas}) => {
-    const ninjaList = ninjas.map(ninja => {
-        return (
-        <div className='ninja' key={ninja.id}>
-            <div>Name: { ninja.name }</div>
-            <div>Age: { ninja.age }</div>
-            <div>Belt: { ninja.belt }</div>
-        </div>
-        )
-    })
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20){
+    //     return (
+    //     <div className='ninja' key={ninja.id}>
+    //         <div>Name: { ninja.name }</div>
+    //         <div>Age: { ninja.age }</div>
+    //         <div>Belt: { ninja.belt }</div>
+    //     </div>
+    //     )
+    // } else {
+    //         return null
+    //     }
+    // })
+
+    
     return(
         <div className='ninja-list'>
-            { ninjaList }
+          {
+            ninjas.map(ninja => {
+                return ninja.age > 20 ? (
+                    <div className='ninja' key={ninja.id}>
+                        <div>Name: { ninja.name }</div>
+                        <div>Age: { ninja.age }</div>
+                        <div>Belt: { ninja.belt }</div>
+                     </div>
+            ) : null
+    })
+                
+            }
         </div>
         )
 }
