856bf4a1ce0c6b51b2520c04c1d1b788aa836087
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-13 00:07:34 -0400

    added Ninjas.js and imported into App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first React App</h1>
-      <p>Welcome</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas />
+      </div>
+      );
+  }
 }
 
 export default App;
