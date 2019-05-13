bdfd57474db5f0e42e0eb010b03bfa6caa24e30c
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-13 00:28:00 -0400

    props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
       <div className='App'>
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
-        <Ninjas />
+        <Ninjas name='Ryu' age='25' belt='black'/>
+         <Ninjas name='Yoshi' age='20' belt='yellow'/>
       </div>
       );
   }
