d17d8847b9475ee25ca665c1a59b21bba1a88989
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-16 00:45:07 -0400

    completed video 24

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 class App extends Component {
   state = {
@@ -16,11 +17,19 @@ class App extends Component {
       todos
     })
   }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo]; //creating copy of the array
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
       <div className='todo-app container'>
         <h1 className='center blue-text'>Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
       );
   }
